af089de13a02b57fc68c5160753f824f4c3763a1
jon989 <jong989@gmail.com>
2015-02-12 10:33:55 -0700

    start

---
+++ tabler/Models/Table.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tabler.Models
+{
+    public enum Status
+    {
+        open,
+        seated,
+        dirty
+    }
+    public class Table
+    {
+        public int TableID { get; set; }
+        public Status status { get; set; }
+        public DateTime timeStamp { get; set; }
+        public string section { get; set; }
+    }
+}

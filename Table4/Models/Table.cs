80b966bcec8a130ad5adbb67db66e08ef33d9c48
jon989 <jong989@gmail.com>
2015-03-10 15:07:52 -0700

    linkedbuttonstodatabase

---
+++ Table4/Models/Table.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Table4.Models
+{
+    public class Table
+    {
+        public int id { get; set; }
+        public int number { get; set; }
+        public int status { get; set; }
+        public string tablename { get; set; }
+    }
+}
---
+++ Table4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Table4.Startup))]
+namespace Table4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
